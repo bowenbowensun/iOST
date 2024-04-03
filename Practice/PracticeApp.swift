ffa88f30100875427772e65f47cbc1f43c4069b0
Bowen Sun <bowensun7@me.com>
2024-04-02 21:18:11 -0400

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Bowen Sun on 2024-04-02.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
